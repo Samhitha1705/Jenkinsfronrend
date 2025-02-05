72e9a9ac63240404ffd88bce1e435fec8539679c
Samhitha1705 <97188512+Samhitha1705@users.noreply.github.com>
2025-02-05 17:02:06 +0530

    Update DocumentUploadForm.jsx

--- src/EmployeeComponents/DocumentUploadForm.jsx
+++ src/EmployeeComponents/DocumentUploadForm.jsx
@@ -62,7 +62,7 @@ const DocumentUploadForm = ({ formData, onNext, onBack, onCancel, onFormDataChan
 
         try {
             const token = localStorage.getItem('token');
-            const response = await axios.post('https://dobbyfrontend.azurewebsites.net/api/v1/employeeManager/add', formDataToSend, {
+            const response = await axios.post('https://dobbybackend.azurewebsites.net/api/v1/employeeManager/add', formDataToSend, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
 
